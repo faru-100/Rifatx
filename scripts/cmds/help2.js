1d658f7687627f63272b524d61a6457bc83f8ec7
SPIKE-CYBER <148491395+SPIKE-777@users.noreply.github.com>
2025-01-10 23:56:38 +0600

    Update help2.js

--- scripts/cmds/help2.js
+++ scripts/cmds/help2.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🤍| ❁𝙏𝘼𝙈𝙄𝙈𖣘 𝘼𝙄 ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
+const doNotDelete = "[ 🤍| ❁𝗥𝗜𝗙𝗔𝗧 𝗕𝗢𝗧𖣘𝗩𝟮  ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
 
 module.exports = {
 	config: {
